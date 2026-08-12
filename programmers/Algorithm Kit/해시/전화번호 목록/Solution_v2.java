44e85b238c8dbdce613fceda42046a3fd9ab7106
jyvnee <jyunee54@gmail.com>
2026-08-12 10:23:40 +0900

    programmers: 전화번호 목록

---
+++ programmers/Algorithm Kit/해시/전화번호 목록/Solution_v2.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public boolean solution(String[] phone_book) {
+        Set<String> phoneSet = new HashSet<>();
+        
+        for (String phone : phone_book) {
+            phoneSet.add(phone);
+        }
+        
+        for (String phone : phone_book) {
+            for (int i=1; i<phone.length(); i++) {
+                String prefix = phone.substring(0, i);
+                
+                if (phoneSet.contains(prefix)) {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+}
