9f8cba72d595debe7a91fb733552c05455960eb5
jyvnee <jyunee54@gmail.com>
2026-08-18 14:10:35 +0900

    programmers: 의상

---
+++ programmers/Algorithm Kit/해시/의상/Solution_v1.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        Map<String, Integer> map = new HashMap<>();
+        
+        for (String[] clothe: clothes) {
+            map.put(clothe[1], map.getOrDefault(clothe[1], 0) + 1);
+        }
+        
+        int answer = 1;
+        for (int value : map.values()) {
+            answer *= (value + 1);
+        }
+            
+        return answer-1;
+    }
+}
